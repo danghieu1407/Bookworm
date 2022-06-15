c4be902fa56f49ca84440aa01eb471e5d433f9d7
danghieu1407 <danghieu140701@gmail.com>
2022-06-16 02:06:49 +0700

    pagination of listing prouduct in shoppage

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,19 +6,34 @@ use Illuminate\Http\Request;
 use App\Models\Book;
 
 class ShopController extends Controller
-{
+{   
+    //pagination
+    const LIMIT_DEFAULT = 12;
+    const PAGE_INDEX_DEFAULT = 1;
+
+
     protected $bookModel;
     public function __construct(Book $bookModel)
     {
         $this->bookModel = $bookModel;
     }
 
-    public function listingBooks()
+    public function listingBooks(Request $params)
     {
-        dd('hgello');
+        //pagination
+        $pageIndex = $params['pageIndex'] ?? self::PAGE_INDEX_DEFAULT;
+
+        $limit = $params['limit'] ?? self::LIMIT_DEFAULT;
+        $offset = ($pageIndex - 1) * $limit;
+        //querry
         $books = $this->bookModel->with('Category')->get();
-        
-        return $books;
+        $items = $books->slice($offset, $limit);
+        return [
+            'items' => $items,
+            'total' => $books->count(),
+            'pageIndex' => $pageIndex,
+            'limit' => $limit,
+        ];
     }
     
 
